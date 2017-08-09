8dc07b02902f8edf55ec22843271512568b07736
Tristan Maske <trismaske@gmail.com>
2017-08-09 21:36:47 +0200

    Changed exit to GameState

--- include/Game.hpp
+++ include/Game.hpp
@@ -14,19 +14,28 @@
 #include <sstream>
 #include <string>
 
+clas enum GameState
+{
+	PLAY,
+	EXIT,
+	LOAD,
+	SET,
+	SAVE
+};
+
 class Game {
 	public:
 		Game();
 		Game(Game const & src);
 		~Game();
 		Game & 					operator=(Game const & src);
-		bool					getExit() const;
-		void					setExit(const bool newExit);
+		GameState				getState() const;
+		void					setState(const GameState newState);
 		int						getGameInput() const;
 		void					setGameInput(const int newInput);
 		Settings				getSettings() const;
 		void					setSettings(const Settings newSettings);
-		Character	*			getPlayer() const;
+		Character				*getPlayer() const;
 		void					setPlayer(Player *newPlayer);
 		std::vector<Character*>	getEnemies() const;
 		void					setEnemies(const std::vector<Character*> newEnemies);
@@ -43,7 +52,7 @@ class Game {
 		void					updateGameData();
 
 	private:
-		bool					_exit;
+		GameState				_state;
 		int						_gameInput;
 		Settings				_settings;
 		Character				*_player;
