34fe2fb95433a8b6e00c0e9069a361d94b15a998
Tristan MASKE <tmaske@e4r6p4.wethinkcode.co.za>
2017-09-29 12:19:40 +0200

    install script hopefully working now

--- .install.sh
+++ .install.sh
@@ -1,15 +1,7 @@
 if [ ! -d "$HOME/.brew" ]; then
 	sh -c "$(curl -fsSL https://raw.githubusercontent.com/Tolsadus/42homebrewfix/master/install.sh)"
-	osascript -e 'tell application "iTerm2"
-		quit
-	    make new terminal
-	    tell the current terminal
-	        activate current session
-	        launch session "Default Session"
-	        tell the last session
-	            write text "cd ~/Bomberman; sh .cRun.sh;"
-	        end tell
-	    end tell
+	osascript -e 'tell application "iTerm2
+    create window with default profile command "sh ~/Bomberman/.cRun.sh -hold"
 	end tell'
 fi
 sh .cRun.sh
--- .restartTerm.scpt
+++
@@ -1,4 +0,0 @@
-tell application "iTerm2"
-	quit
-	create window with default profile command "sh .cRun.sh"
-end tell
