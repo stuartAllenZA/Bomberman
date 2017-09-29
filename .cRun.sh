1a04c62fa634fa8d66ebd1c6eee977024fdd61f6
Tristan MASKE <tmaske@e4r5p1.wethinkcode.co.za>
2017-09-29 11:29:40 +0200

    install scripts

--- .cRun.sh
+++ .cRun.sh
@@ -1,7 +1,3 @@
-if [ ! -d "$HOME/.brew" ]; then
-	sh -c "$(curl -fsSL https://raw.githubusercontent.com/Tolsadus/42homebrewfix/master/install.sh)"
-fi
-osascript .restartTerm.scpt
 if [ ! -d "$HOME/.brew/Cellar/cmake" ]; then
 	brew install cmake
 fi
